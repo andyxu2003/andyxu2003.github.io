9eae1714bbce82dae04542a5bce441da3e960b84
Andy Xu <123328675+andyxu2003@users.noreply.github.com>
2024-08-27 01:48:02 -0400

    reworked headers test

--- javascript/header-footer.js
+++ javascript/header-footer.js
@@ -1,4 +1,8 @@
 $(function () {
-    $("#header").load("../header.html");
+    $("#home-header").load("../headers/home-header.html");
+    $("#albums-header").load("../headers/albums-header.html");
+    $("#gallery-header").load("../headers/gallery-header.html");
+    $("#videography-header").load("../headers/videography-header.html");
+    $("#about-header").load("../headers/about-header.html");
     $("#footer").load("../footer.html");
 });
