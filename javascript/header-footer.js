ccb34e1bfca55e4f1d8794686193d17f52800062
Andy Xu <xuandy@live.com>
2025-01-17 17:00:41 -0500

    website rework

--- javascript/header-footer.js
+++ javascript/header-footer.js
@@ -1,8 +1,4 @@
 $(function () {
-    $("#home-header").load("../headers/home-header.html");
-    $("#albums-header").load("../headers/albums-header.html");
-    $("#gallery-header").load("../headers/gallery-header.html");
-    $("#videography-header").load("../headers/videography-header.html");
-    $("#about-header").load("../headers/about-header.html");
+    $("#header").load("../header.html");
     $("#footer").load("../footer.html");
 });
